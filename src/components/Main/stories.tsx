2122ea45cf8a484673545086cfa7439b0b27a7f9
Francisco Martin <francisco.martin@ambevtech.com.br>
2021-03-21 00:02:03 -0300

    change knobs to storybook controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,16 +1,19 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'default',
+    description: 'description default'
+  }
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text('Description', 'texto de descrição')}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
+Basic.args = {
+  title: 'basic',
+  description: 'description basic'
+};
+
+export const Default: Story = (args) => <Main {...args} />;
