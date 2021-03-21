84fe76daa7ed78ecabd94390ec13383258091703
Francisco Martin <francisco.martin@ambevtech.com.br>
2021-03-20 23:37:39 -0300

    add storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,19 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import React from 'react';
+import * as S from './styles';
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, React, NextJs'
+}) => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem de um átomo e React Avançado" />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="desenvolvedor em frente a tela"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
